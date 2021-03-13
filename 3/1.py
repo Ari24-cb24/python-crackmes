f56a70c3ba3da056b554b6d294830c634310ebe4
Ari <56089155+Ari24-cb24@users.noreply.github.com>
2021-03-13 19:58:41 +0100

    Add files via upload

---
+++ 3/1.py
@@ -0,0 +1,5 @@
+s="  _  __\n | |/ /\n | ' / ___ _   _ _ __   __ _ ___ ___ \n |  < / _ \\ | | | '_ \\ / _` / __/ __|\n | . \\  __/ |_| | |_) | (_| \\__ \\__ \\\n |_|\\_\\___|\\__, | .__/ \\__,_|___/___/\n            __/ | | \n           |___/|_|\n";import sys;a="71948911191009749489669459759519121933";xyz=sys;xzy=xyz.stdout;zxy=xzy.write;xy=lambda s: zxy(s);xy(s);b=input("\n"+"E"+"n"+"t"+"e"+"r"+" "+"c"+"o"+"r"+"r"+"e"+"c"+"t"+" "+"p"+"a"+"s"+"s"+"w"+"o"+"r"+"d"+":"+(""*100)+" ");b=map(lambda k: ord(k),b);d=list(b);e=[a]+d;l,g=e+d,d
+if("9".join([str(t)for t in g])).split(str(5+5-1)).__eq__(a.split(str(20+5+4)[1])):xy("Masterkey correct! Welcome Josh to your Keepass system!");sys.exit(0)
+else:xy("Try again next time, Josh!");sys.exit(-1)
+
+
