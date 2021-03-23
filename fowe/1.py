3f0528ab492ef54c3ad817c2148942e1c8949fb5
Ari <56089155+Ari24-cb24@users.noreply.github.com>
2021-03-23 18:25:54 +0100

    Add files via upload

---
+++ fowe/1.py
@@ -0,0 +1,14 @@
+exec("i"+"m"+"p"+"o"+"r"+"t"+" "+"m"+"a"+"t"+"h"+","+"b"+"a"+"s"+"e"+"6"+"4")
+z=math;e=base64;g=e.b64encode;y=z.factorial;k=int;h=e.b64decode
+b=input;o=print;c=o;p=z.pow;del o;d=lambda s: int(s);globals().update(l=chr,e=range)
+a=b((chr(32).replace(chr(32),"")).join(l(n)for n in[69,110,116,101,114,32,105,116,101,114,97,116,105,111,110,115,58,32]))
+while not(int(a.isnumeric()).__eq__(int(not False))):
+ c(h(b'U'+b'G'+b'x'+b'l'+b'Y'+b'X'+b'N'+b'l'+b'I'+b'G'+b'V'+b'u'+\
+  b'd'+b'G'+b'V'+b'y'+\
+  b'I'+\
+  b'G'+b'E'+b'g'+b'd'+b'm'+b'F'+b's'+b'a'+b'W'+b'Q'+b'g'+b'b'+b'n'+b'V'+b't'+b'Y'+\
+  b'm'+b'V'+b'y'+b'I'+b'Q'+b'='+b'=').decode("u"+"t"+"f"+"-"+"8"),"\n")
+ a=b((chr(32).replace(chr(32),"")).join(l(n)for n in[69,110,116,101,114,32,105,116,101,114,97,116,105,111,110,115,58,32]))
+del z;c();a=d(a);f=(int(ord("!")).__add__((int(ord("J")).__sub__(int((ord("$")))+int(ord("&"))+int(ord("!"))))))**(ord("c")-98)
+g=y(f)
+for n in e(a):h=g;g+=f;f=h;del h;c(g)
